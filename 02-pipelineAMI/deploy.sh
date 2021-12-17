83ace3dd50ae78d717d4d1c592f44d8894d00d77
Ubuntu <daniellesilveira@outlook.com>
2021-12-17 23:28:44 +0000

    ok

--- 02-pipelineAMI/deploy.sh
+++ 02-pipelineAMI/deploy.sh
@@ -2,7 +2,8 @@
 
 # export TF_VAR_resource_id="~dddss"
 
-cd 02-pipelineAMI/terraform
+# cd 02-pipelineAMI/terraform
+cd terraform
 terraform init
 terraform apply -auto-approve
 
@@ -10,6 +11,7 @@ echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-img]" > ../ansible/hosts # cria arquivo
+# echo $"15.228.79.86" >> ../ansible/hosts # cria arquivo
 echo "$(terraform output | grep public_ip | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
@@ -17,5 +19,5 @@ sleep 10 # 10 segundos
 
 cd ../ansible
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/key-private-turma3-dani-dev.pem ]"
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/key-private-turma3-dani-dev ]"
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/key-private-turma3-dani-dev.pem
